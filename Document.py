28e382f86538e7d1803ab7d16eecb7b57b214de4
Moses Madavaram <mosesgab@gmail.com>
2024-05-07 19:18:13 -0400

    first commit

---
+++ Document.py
@@ -0,0 +1,72 @@
+import pandas
+import Quandl, math, datetime
+import numpy as np
+from sklearn import preprocessing, cross_validation, svm
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+style.use('ggplot')
+
+df = Quandl.get('WIKI/GOOGL')
+df = df[['Adj. Open', 'Adj. High','Adj. Close','Adj. Volume',]]
+df['HL_PCT'] = (df['Adj. High'] - df['Adj. Close']) / df['Adj/ Close'] * 100.0
+df['PCT_change'] = (df['Adj. Close'] - df['Adj. Open']) / df['Adj. Open'] * 100.0
+
+df = df[['Adj. Close', "HL_PCT","PCT_change","Adj. Volume"]]
+
+
+
+forecast_col = "Adj. Close"
+df.fillna(99999999, inplace=True)
+
+forecast_out = int(Math.ceil(0.01* len(df)))
+print(forecast_out)
+
+df['label'] = df[forecast_col].shift(-forecast_out)
+
+
+
+
+
+X = np.array(df.drop(['label'], 1))
+x = preprocessing.scale(X)
+X = X[:-forecast_out]
+X_lately = X[-forecast_out:]
+
+
+df.dropna(inplace=True)
+y = np.array(df['label'])
+y = np.array(df['label'])
+
+
+X_train, X_test, y_train, y_test = cross_validation.train_test_split(X,y,test_size = 0.2) #20% ofthe data to test
+
+clf = LinearRegression(n_jobs=-1)
+clf.fit(X_train, y_train)
+accuracy = clf.score(X_test, y_test)
+forecast_set = clf.predict(X_lately)
+print(forecast_set, accuracy, forecast_out)
+df['Forecast'] = np.nan
+
+
+last_date = dr.iloc[-1] #last date
+last_unix = last_date.timestamp()
+one_day = 60 * 60 * 24
+
+next_unix = one_day + last_unix
+
+
+for i in forecast_set:
+    next_date = datetime.datetime.fromtimestamp(next_unix)
+    next_unix += one_day
+    df.loc['next_date'] = [np.nan for x in range(len(df.columns)-1) + [i]]
+
+print(df.head())
+
+df['Adj. Close'].plot()
+df['Forecast'].plot()
+plt.legend(loc=4)
+plt.xlabel('Date')
+plt.ylabel('price')
+plt.show()
